2bfd71871748e873b820a862f53fb2e712850560
Segundobenito <sbenito@udesa.edu.ar>
2021-06-17 20:43:21 -0300

    detail y search

--- javascipt/detail-album.js
+++ javascipt/detail-album.js
@@ -1,16 +1,19 @@
+// let querystring = location.search;
+// let datos = new URLSearchParams (querystring);
+// let mainAlbums = datos.get('id')
+
 fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127")
-.then(function(response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
 
-    let albums = data;
+        let frontTitle = document.querySelector('.title');
 
-    let albumes = document.querySelector(".detailAlbums");
+        frontTitle.innerHTML += data.title
 
-    for (let i = 0; i < 10; i++) {
-        albumes.innerHTML = '<article class="lista_canciones"><p class="nombre_cl_alb"><a href="detail-track.html">'+albums[i]+'</a></p><p class="reproducciones_alb">70.219.630 </p></article>'
+        let albumes = document.querySelector(".lista");
+        albumes.innerHTML += lorem
 
-    }
-})
+    })
---
+++ javascipt/search-results.js
@@ -0,0 +1,7 @@
+let querystring = location.search
+let querystringobj = new URLSearchParams(querystring);
+let busqueda = querystringobj.get("search");
+
+let titulo = document.querySelector('.titulo')
+
+titulo.innerHTML += 'Resultado de busqueda:"'+busqueda+'"'
