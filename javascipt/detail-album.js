ee85abbcdc3821f6aaa533a44c7b598a82d68d13
Segundobenito <sbenito@udesa.edu.ar>
2021-06-17 19:00:38 -0300

    comment

--- javascipt/detail-album.js
+++ javascipt/detail-album.js
@@ -5,11 +5,12 @@ fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127")
 .then(function (data) {
     console.log(data);
 
-    let albums = data.e;
+    let albums = data;
 
     let albumes = document.querySelector(".detailAlbums");
 
     for (let i = 0; i < 10; i++) {
-        albumes.innerHTML = '<article class="lista_canciones"><p class="nombre_cl_alb"><a href="detail-track.html">'+albums[i].tracks+'</a></p><p class="reproducciones_alb">93.473.120 </p></article>'
+        albumes.innerHTML = '<article class="lista_canciones"><p class="nombre_cl_alb"><a href="detail-track.html">'+albums[i]+'</a></p><p class="reproducciones_alb">70.219.630 </p></article>'
+
     }
 })
