f3edc4dc4418a2cef69b32e7ee3c981fc45bb415
GPreuss <GPreuss@udesa.edu.ar>
2021-06-18 17:32:56 -0300

    home

--- javascipt/index.js
+++ javascipt/index.js
@@ -14,16 +14,33 @@
 
 
         for (let i = 0; i < 5; i++) {
-            canciones.innerHTML += '<article><img class="imagen" src="' + songs[i].album.cover_big + '" alt="' + songs[i].title + '"><h3class="nombre_artista"><a href="detail-track.html">' + songs[i].title + '</a></h3></article>'
+            canciones.innerHTML +=`
+                <article>
+                    <img class="imagen" src="${songs[i].album.cover_big}" alt="${songs[i].title}">
+                    <h3class="nombre_artista">
+                        <a href="detail-track.html?id=${songs[i].id}">${songs[i].title}</a>
+                    </h3>
+                </article>`
         }
 
         for (let i = 0; i < 5; i++) {
-            albumes.innerHTML += '<article><img class="imagen" src="' + albums[i].cover_big + '" alt="' + albums[i].title + '"><h3 class="nombre_artista" id="bob"><a href="detail-artist.html">' + albums[i].title + '</a></h3></article>'
+            albumes.innerHTML += `
+                <article>
+                    <img class="imagen" src="${albums[i].cover_big}" alt="${albums[i].title}">
+                    <h3 class="nombre_artista">
+                        <a href="detail-artist.html?id=${albums[i].id}">${albums[i].title}</a>
+                    </h3>
+                </article>`
         }
 
         for (let i = 0; i < 5; i++) {
-            artistas.innerHTML += '<article><img class="imagen" src="' + artists[i].picture_big + '" alt="' + artists[i].name + '"><h3 class="nombre_artista" id="metalica"><a href="detail-artist.html">' + artists[i].name + '</a></h3></article>'
-
+            artistas.innerHTML += `
+            <article>
+                <img class="imagen" src="${artists[i].picture_big}" alt="${artists[i].name}">
+                <h3 class="nombre_artista">
+                    <a href="detail-artist.html?id=${artists[i].id}">${artists[i].name}</a>
+                </h3>
+            </article>`
          }
         })
     .catch(function(error){
