fed1eed8878f653589a12e0d5d386b4ea8920d35
GPreuss <GPreuss@udesa.edu.ar>
2021-06-24 16:39:33 -0300

    jajajaj

--- javascipt/detail-artist.js
+++ javascipt/detail-artist.js
@@ -11,44 +11,32 @@ console.log (url)
     console.log(data);
     let nombre = data.name
     let imagen = data.picture_big
-    let canciones = data.tracklist
     let ubic = document.querySelector('main')
-    console.log(canciones)
     ubic.innerHTML +=`
     <img class="imagen_artista" src="${imagen}" alt="${nombre}">
     <p class="categoria">Artista</p>
     <h1 class="titulo">${nombre}</h1>
-    <h3 class="titulo_lista">Top 5 Canciones :</h3>
-    <article class="menu_lista">
-        <p class="posicion_menu">#</p>
-        <p class="imagen_menu">Album</p>
-        <p class="nombre_menu">Nombre</p>
-        <p class="reproducciones_menu">Duracion </p>
-    </article>
+    <h3 class="titulo_lista">Albumes :</h3>
     <ol class="lista">
     <ol/>
     `
-    fetch(`https://cors-anywhere.herokuapp.com/${canciones}`)
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${codigo}/albums`)
     .then(function(respuesta){
         return respuesta.json();
      })
     .then(function (data) {
-
+        let album = data.data
         let ubicLista = document.querySelector('ol')
 
         for (let i = 0; i < 5; i++) {
-            let nomCancion = data.data[i].title
-            let duracion = data.data[i].duration
-            let fotoAlbm = data.data[i].album.cover_medium
-            let nombreAlbm = data.data[i].album.title
-            let idcancion = data.data[i].id
+            let fotoAlbm = album[i].cover_medium
+            let nombreAlbm = album[i].title
+            let idAlbm = album[i].id
             ubicLista.innerHTML +=`
             <li>
             <article class="lista_canciones">
-            <p>${i+1}</p>
-            <a href="detail-track.html?id=${idcancion}><img class="imagen_all" src="${fotoAlbm}" alt="${nombreAlbm}"></a>
-            <p class="nombre_cl"><a href="detail-track.html?id=${idcancion}">${nomCancion}</a></p>
-            <p class="reproducciones">${duracion}sec </p>
+                <img class="imagen_all" src="${fotoAlbm}" alt="${nombreAlbm}">
+                <p class="nombre_cl"><a href="detail-track.html?id=${idAlbm}">${nombreAlbm}</a></p>
             </article>
             </li> 
             `
