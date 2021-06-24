ec7e370aa0aeb3c91b5347fcbf664a3118485827
GPreuss <GPreuss@udesa.edu.ar>
2021-06-24 15:54:32 -0300

    hahahaha

--- javascipt/detail-generes.js
+++ javascipt/detail-generes.js
@@ -1,14 +1,34 @@
 let search_results = new URLSearchParams(this.location.search);
 let codigo = search_results.get('id');
 console.log('id:' + codigo);
-let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${codigo}`
 
-fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${codigo}`)
+fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${codigo}`)
  .then(function(respuesta) {
     return respuesta.json();
  })
  .then(function (data) {
     console.log(data);
+    let nombreGenero = data.data[i].name
+    
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${codigo}/artist`)
+    .then(function(respuesta) {
+       return respuesta.json();
+    })
+    .then(function (data) {
+       console.log(data);
+    for (let i = 1; i < genero.length; i++) {
+        let fotoArtista = genero[i].picture_big
+        let nombreArtista = genero[i].name
+        let idArtista = genero[i].id
+        generes.innerHTML += `
+        <article>
+            <img class="imagen" src="${fotoGenero}" alt="${nombreGenero}">
+            <h3 class="nombre_artista">
+                <a href="detail-generes.html?id=${idGenero}">${nombreGenero}</a>
+            </h3>
+        </article>`
+    }
+    })
  })
  .catch(function (error) {
     console.log(error);
--- javascipt/index.js
+++ javascipt/index.js
@@ -18,9 +18,9 @@
                 <article>
                     <img class="imagen" src="${songs[i].album.cover_big}" alt="${songs[i].title}">
                     <h3class="nombre_cancion">
-                        <a href="detail-track.html?id=${songs[i].id}">${songs[i].title}</a>
+                        <a href="detail-track.html?id=${songs[i].id}">${songs[i].title}, </a><a href="detail-artist.html?id=${songs[i].artist.id}">${songs[i].artist.name}</a>
                     </h3>
-                    <h3><a href="detail-artist.html?id=${albums[i].id}">${albums[i].title}</a>
+                    <h3>
                     </h3>
                 </article>`
         }
