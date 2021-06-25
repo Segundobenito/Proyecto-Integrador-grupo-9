31e00aeeb29aa7b108d3e1f415f6647c0b759154
GPreuss <GPreuss@udesa.edu.ar>
2021-06-25 13:25:39 -0300

    mi playlist

--- javascipt/detail-track.js
+++ javascipt/detail-track.js
@@ -1,24 +1,23 @@
-
 let search_results = new URLSearchParams(this.location.search);
 let codigo = search_results.get('id');
 console.log('id: ' + codigo);
 
 fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${codigo}`)
- .then(function(respuesta) {
-    return respuesta.json();
- })
- .then(function (data) {
-    console.log(data);
-    let nombre = data.title
-    let nombreArtist = data.artist.name
-    let artistaId = data.artist.id
-    let fotoArtista = data.artist.picture
-    let ubic = document.querySelector('main')
-    let nombreAlbm = data.album.title
-    let albmId = data.album.id
-    let fotoAlbm = data.album.cover_xl
+    .then(function (respuesta) {
+        return respuesta.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        let nombre = data.title;
+        let nombreArtist = data.artist.name;
+        let artistaId = data.artist.id;
+        let fotoArtista = data.artist.picture;
+        let ubic = document.querySelector('main');
+        let nombreAlbm = data.album.title;
+        let albmId = data.album.id;
+        let fotoAlbm = data.album.cover_xl;
 
-    ubic.innerHTML +=`
+        ubic.innerHTML = `
         <iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/track/${codigo}" width="100%" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>
         <p class="categoria">Cancion</p>
         <article class="titulo_track">
@@ -36,8 +35,44 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${codigo
                 <p><a href="playlist.html">Ver Mi Playlist</a></p>
             </article>
         </section>
-    `
- })
- .catch(function (error) {
-    console.log(error);
-})
+    ` + ubic.innerHTML
+
+        let miPlaylist = [];
+        console.log(window.localStorage);
+
+        let traigoPlaylist = window.localStorage.getItem('miPlaylist');
+        console.log(traigoPlaylist);
+
+        if (traigoPlaylist == null) {
+            miPlaylist = [];
+            console.log(miPlaylist);
+        } else {
+            miPlaylist = JSON.parse(traigoPlaylist);
+        }
+
+        if (miPlaylist.includes(codigo)) {
+            document.querySelector('.agregar').innerHTML = "Quitar de Mi Playlist";
+        }
+
+        let add = document.querySelector('.agregar');
+        console.log(add);
+
+        add.addEventListener('click', function () {
+            if (miPlaylist.includes(codigo)) { //aca buscamos si la cancion esta en la playlist y si l esta, la sacamos y cambia el texto de el boton
+                let indice = miPlaylist.indexOf(codigo); //si la cancion esta devuelve 1
+                miPlaylist.splice(indice, 1); //splice se va a ocuar de sacar la cancion del array, si indice es 1
+                add.innerHTML = "Agregar a Mi Playlist";
+                console.log(miPlaylist);
+            } else { // Implica que la cancion no esta en la playlis, por lo tanto la agregamos y cambiamos el texto del boton
+                miPlaylist.push(codigo);
+                add.innerHTML = "Quitar de Mi Playlist";
+            }
+            let miPlaylistGuardada = JSON.stringify(miPlaylist); //paso mi playlist a string para poder guardarlo en el local storage
+            console.log(miPlaylistGuardada);
+            localStorage.setItem('miPlaylist', miPlaylistGuardada); // aca se manda la playlist al local storage
+            console.log(localStorage);
+        })
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
