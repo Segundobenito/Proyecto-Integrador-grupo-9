8e9d26a7570edb81d598137abc0e0947d5e90725
GPreuss <GPreuss@udesa.edu.ar>
2021-06-25 13:28:30 -0300

    piola

--- javascipt/detail-track.js
+++ javascipt/detail-track.js
@@ -2,6 +2,19 @@ let search_results = new URLSearchParams(this.location.search);
 let codigo = search_results.get('id');
 console.log('id: ' + codigo);
 
+let formulario = document.querySelector("form");
+let coso = document.getElementById("fomu");
+formulario.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (fomu.value === '') {
+        alert('EL CAMPO NO PUEDE ESTAR VACIO')
+    } else if (fomu.value <= 3) {
+        alert('Mas que 3 please');
+    } else {
+        formulario.submit();
+    }
+})
+
 fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${codigo}`)
     .then(function (respuesta) {
         return respuesta.json();
